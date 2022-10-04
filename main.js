e5463638995ab07b39e2a50902951e28274fa8e1
Nigar Alizada <nalizade96@gmail.com>
2022-10-04 21:34:49 +0400

    app

---
+++ main.js
@@ -0,0 +1,42 @@
+let weather = {
+    apiKey: "ee0cf279b939c11c634d4cd308145cc7",
+    fetchWeather: function(city){
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function(data){
+        //!Destructuring data from object
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+
+        //!Inserting destructured data to needed fields
+        document.querySelector(".city").innerText = `Weather in ${name}`;
+        document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`;
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = `${temp} °C`;
+        document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+        document.querySelector(".wind").innerText = `Wind speed: ${speed}km/h`;
+
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function(){
+       let input = document.querySelector(".search-bar");
+       this.fetchWeather(input.value);
+    }
+}
+
+document.querySelector(".search button").addEventListener('click', function(){
+    weather.search();
+})
+
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if (event.key == "Enter"){
+        weather.search();
+    }
+})
+
