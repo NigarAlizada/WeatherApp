85ba31c4ebae6ee48e566e9a6a6c6608e1c94112
Nigar Alizada <nalizade96@gmail.com>
2022-10-10 14:27:40 +0400

    WeatherAppVanillaJS

--- main.js
+++ main.js
@@ -33,7 +33,6 @@ document.querySelector(".search button").addEventListener('click', function(){
     weather.search();
 })
 
-
 document.querySelector(".search-bar").addEventListener("keyup", function(event){
     if (event.key == "Enter"){
         weather.search();
